ddab86cb03a5e22eca3e5f8dc5fccd8fa196c53a
Alexander Cobleigh <cblgh@cblgh.org>
2017-07-08 16:19:42 +0200

    clean up hyperdungeon a bit

--- hyperdb-example/hyperdungeon.js
+++ hyperdb-example/hyperdungeon.js
@@ -5,39 +5,36 @@ var pages = require("random-access-page-files")
 var raf = require("random-access-file")
 var ram = require("random-access-memory")
 
-var st = process.argv.indexOf("--ram") > -1 ? ram : storage
-var optimized = process.argv.indexOf("--optimized") > -1
+var st = process.argv.indexOf("--storage") > -1 ? storage : ram;
+var optimized = process.argv.indexOf("--optimized") > -1;
 
 var local = hypercore("./dungeon-dir", {valueEncoding: "json", sparse: true})
 var db = hyperdb([
-  // st <- <dat:hash>: put what you sync from <dat:hash> into the storage st
-  // hypercore(st, "48e2619899edb24f4d5031b5e0cf16e6caef0cc20710c8c60783428f4e8d2ef3", {valueEncoding: "json", sparse: true}), // mafintosh
-  hypercore(st, "cd1034cedfe2dccdd225a96abcf0a5576158426ddd6078c2f89aa352da77115d", {valueEncoding: "json", sparse: true}), // wintermute
-  // hypercore(st, "4ae572a70f1950152a0fe3ead0d997b2bf1a29af083f7dfacfa3b3b6e2972b48", {valueEncoding: "json", sparse: true}), // macbook
-  local
+    // st <- <dat:hash>: put what you sync from <dat:hash> into the storage st
+    // hypercore(st, "48e2619899edb24f4d5031b5e0cf16e6caef0cc20710c8c60783428f4e8d2ef3", {valueEncoding: "json", sparse: true}), // mafintosh
+    // hypercore(st, "cd1034cedfe2dccdd225a96abcf0a5576158426ddd6078c2f89aa352da77115d", {valueEncoding: "json", sparse: true}), // wintermute
+    // hypercore(st, "4ae572a70f1950152a0fe3ead0d997b2bf1a29af083f7dfacfa3b3b6e2972b48", {valueEncoding: "json", sparse: true}), // macbook
+    local
 ])
 
 var direction = process.argv[2] || "north"
 
 db.ready(function () {
-  var id = local.key.toString("hex");
-  console.log("local key", id);
-  var sw = hyperdiscovery(db, {live: true})
-  if (process.argv.indexOf("--sync") > -1) {
-    db.feeds[0].download({start: 0, end: -1})
-    return
-  }
+    var id = local.key.toString("hex");
+    console.log("local key", id);
 
-  console.log("Joining swarm ...")
+    var sw = hyperdiscovery(db, {live: true})
 
-  db.feeds[0].get(0, function () {
-    // haxx to make sure we have a connection lol
-    var now = Date.now()
+    if (process.argv.indexOf("--sync") > -1) {
+        db.feeds[0].download({start: 0, end: -1})
+        return
+    }
 
-    console.log("Swarm joined!");
-    db.feeds[0].on("download", function (index) {
-      console.log("Downloaded block", index)
+    sw.on("connection", function(peer, type) {
+        console.log("we got a connection");
     })
+
+    console.log("Joining swarm ...")
 
     // fetch local player's position from hyperdb
     get(id).then(function(position) {
@@ -61,7 +58,7 @@ db.ready(function () {
         // TODO: how do i save information to a tile?
         // if every client saves to the same position, "0-0": "{data: ...}", will i be able to iterate through all
         // of the information, composing a large collection?
-        
+
         // save the local player's new position
         update(id, JSON.stringify(position)).then(function() {
             // update current tile with this player's id
@@ -71,7 +68,6 @@ db.ready(function () {
             // sw.destroy();
         });
     })
-  })
 })
 
 function update(key, val) {
@@ -94,8 +90,7 @@ function get(key) {
                 console.log(err); 
                 resolve(null);
             } else if (nodes && nodes[0]) {
-                // TODO: insert code to check if many nodes have their own value at this spot
-                // console.log("GOT EM");
+                // TODO: insert code to check if many nodes can have their own values for the same key
                 resolve(JSON.parse(nodes[0].value))
             } else {
                 resolve(null);
@@ -103,37 +98,10 @@ function get(key) {
         })
     })
 }
-  // if (process.argv.indexOf("--add") > -1) {
-  //       // try to put in a nonsense word to prove that i can write to my feed
-  //       db.put(word, 1, function (err, nodes) {
-  //           // probably no callback for put, but idk
-  //           console.log("Adding " + word + " to the dictionary!")
-  //           if (err)  console.log(err);
-  //           console.log(nodes)
-  //           console.log("Shutting down swarm ...")
-  //           sw.destroy()
-  //       });
-  // } else {
-  //       db.get(word, function (err, nodes) {
-  //         console.log("Query took", Date.now() - now, "ms")
-  //         console.log()
-  //           
-  //         console.log(nodes);
-  //         if (nodes && nodes[0] && nodes[0].key === word) {
-  //           console.log(""" + word + "" is an English word")
-  //         } else {
-  //           console.log(""" + word + "" is *not* an English word")
-  //         }
-  //
-  //         console.log()
-  //         console.log("Shutting down swarm ...")
-  //         sw.destroy()
-  //       })
-  // }
 
 function storage (name) {
-  if (name === "data") return pages("words.db/data")
-  return raf("dungeon.map2/" + name)
+    if (name === "data") return pages("dungeon.map/data")
+    return raf("dungeon.map/" + name)
 }
 
 function noop () {}
