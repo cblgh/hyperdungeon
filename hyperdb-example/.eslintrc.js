8068e2d0c95e4830b362117ec9e61c1ab606fae2
Alexander Cobleigh <cblgh@cblgh.org>
2017-07-19 23:24:54 +0200

    add eslintrc

---
+++ hyperdb-example/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": "off",
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
